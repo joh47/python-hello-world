879d07ca6ca0c421849c9e17027b8c1ff3271acc
jo_h47 <jo.h47.twitch.tv@gmail.com>
2020-05-17 20:37:05 -0400

    feat(hello.py): add guessing game functionality

--- hello.py
+++ hello.py
@@ -1,1 +1,26 @@
-print("Screw you Windows!")
+from random import randrange
+
+text = input("Guess a number from 1 to 10. \n")
+
+if(not text.isdigit()):
+    print("You did not enter a digit.")
+    exit()
+elif(not int(text) >= 1):
+    print ("Your digit was not at least 1.")
+    exit()
+elif(not (int(text) <= 10)):
+    print ("Your digit was not smaller than 10.")
+    exit()
+else:
+    print("That was a digit between 1 and 10")
+
+print("Your guess is " + text)
+
+random_number = randrange(10) + 1
+
+print("My guess was " + str(random_number))
+
+if(random_number == int(text)):
+    print("Hey rockstar! You got it!")
+else:
+    print("Oops - better luck next time!")
